a2beeffae07c025ebe9d94a04f9424da13acb214
Neil Breeden III <neil@htmlbuffet.com>
2015-01-11 14:50:56 -0800

    Added bower and grunt-bowercopy packages. Setup grunt to copy dependencies to test

--- gruntfile.js
+++ gruntfile.js
@@ -1,15 +1,30 @@
 module.exports = function (grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
- 
+ 	
+	bowercopy: {
+		options: {
+		},
+		test: {
+			options: {
+				destPrefix: 'test/bower'
+			},
+			files: {
+				'require.js': 'requirejs/require.js'
+			}
+		}
+	},
+
         karma: {
             unit: {
                 configFile: 'karma.conf.js'
             }
         }
     });
- 
+
+    grunt.loadNpmTasks('grunt-bowercopy'); 
     grunt.loadNpmTasks('grunt-karma');
-    grunt.registerTask('default', ['karma']);
+    
+    grunt.registerTask('default', ['bowercopy', 'karma']);
 };
 
