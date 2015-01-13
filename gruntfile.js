3c4cecf439cb42bfb5364b062b98c54b9130e435
Neil Breeden III <neil@htmlbuffet.com>
2015-01-12 22:01:20 -0800

    Added grunt configurations for build, dev, and production. Added grunt devserver and watcher. Added sandbox site.

--- gruntfile.js
+++ gruntfile.js
@@ -22,6 +22,14 @@ module.exports = function (grunt) {
                 },
                 files: {
                     "test/smorgasbord-build.css": "src/smorgasbord.less",
+                    "sandbox/smorgasbord-build.css": "src/smorgasbord.less"
+                }
+            },
+            production: {
+                options: {
+                    paths: ["src"]
+                },
+                files: {
                     "dist/smorgasbord.css": "src/smorgasbord.less"
                 }
             }
@@ -31,12 +39,36 @@ module.exports = function (grunt) {
             unit: {
                 configFile: "karma.conf.js"
             }
+        },
+        
+        devserver: {
+            server: {},
+            options: {
+                port: 8080,
+                base: "sandbox/",
+                async: false
+            }
+        },
+        
+        watch: {
+            scripts: {
+                files: ['src/**/*', 'sandbox/**/*'],
+                tasks: ['bowercopy', 'less:development', 'karma'],
+                options: {
+                    spawn: false
+                }
+            }
         }
+        
     });
 
     grunt.loadNpmTasks("grunt-bowercopy");
     grunt.loadNpmTasks("grunt-contrib-less");
     grunt.loadNpmTasks("grunt-karma");
+    grunt.loadNpmTasks("grunt-devserver");
+    grunt.loadNpmTasks("grunt-contrib-watch");
     
-    grunt.registerTask("default", ["bowercopy", "less", "karma"]);
+    grunt.registerTask("default", ["bowercopy", "less:development", "karma"]);
+    grunt.registerTask("production", ["less:production"]);
+    grunt.registerTask("dev", ["bowercopy", "less:development", "karma", "devserver", "watch"]);
 };
