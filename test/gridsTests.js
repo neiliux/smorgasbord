584b5a4af0db2b1df7ce2a304060ba338d6dfb4d
Neil Breeden III <neil@htmlbuffet.com>
2015-01-28 22:18:49 -0800

    Minor cleanup.

--- test/gridsTests.js
+++ test/gridsTests.js
@@ -1,49 +1,48 @@
-/*global define, describe, it, console, beforeEach, afterEach, require */
+/*global document, window, define, describe, it, console, beforeEach, afterEach, require */
 define(["jquery"], function ($) {
     "use strict";
     var expect = require("chai").expect;
-    
+
     // TODO: Move this to helpers.
     function getPseudoStylePropertyValue(selector, pseudoStyle, property) {
         var item = document.querySelector(selector);
         return window.getComputedStyle(item, pseudoStyle)
             .getPropertyValue(property);
     }
-    
+
     describe("Grids", function () {
         beforeEach(function () {
             var gridContainer = $('<div class="grid"></div>');
             gridContainer.append('<div class="grid-h-1-3"></div>');
             $("body").append(gridContainer);
         });
-        
+
         afterEach(function () {
             $("div.grid").remove();
         });
-        
+
         describe('Grid container', function () {
             var selector = "div.grid",
                 propertyKey = "display",
                 expectedDisplayPropertyValue = "table";
-            
+
             describe(":before", function () {
                 var pseudoSelector = ":before";
-                
+
                 it("has clear fix display property applied", function () {
                     var propertyValue = getPseudoStylePropertyValue(selector, pseudoSelector, propertyKey);
                     expect(propertyValue).to.equal(expectedDisplayPropertyValue);
                 });
             });
-            
+
             describe(":after", function () {
                 var psuedoSelector = ":after";
-                
+
                 it("has clear fix display property", function () {
                     var propertyValue = getPseudoStylePropertyValue(selector, psuedoSelector, propertyKey);
                     expect(propertyValue).to.equal(expectedDisplayPropertyValue);
                 });
             });
         });
-        
     });
 });
--- test/main-test.js
+++ test/main-test.js
@@ -1,4 +1,4 @@
-/*global require */
+/*global require, window */
 var allTestFiles = [];
 var TEST_REGEXP = /(tests)\.js$/i;
 
@@ -18,24 +18,20 @@ Object.keys(window.__karma__.files).forEach(function (file) {
 require.config({
     // Karma serves files under /base, which is the basePath from your config file
     baseUrl: '/base',
-
     paths: {
         'jquery': 'test/bower/jquery',
         //'underscore': '../lib/underscore',
         'load_css': 'test/main-css',
         'chai': 'test/bower/chai',
         'chai-jquery': 'test/bower/chai-jquery'
-	},
-
+	  },
     shim: {
         'jquery': {
             exports: '$'
         }
     },
-
     // dynamically load all test files
     deps: ['load_css'].concat(allTestFiles),
-
     // we have to kickoff jasmine, as it is asynchronous
     callback: window.__karma__.start
 });
